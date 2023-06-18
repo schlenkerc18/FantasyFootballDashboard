036d03b0f17d4bebc3bfe6300e67cbf16385b969
Christopher Schlenker <schlenkerc18@hanover.edu>
2023-06-18 17:11:19 -0400

    gcloud stuff

--- app.py
+++ app.py
@@ -21,6 +21,8 @@ app.layout = html.Div([
 	dash.page_container
 ], style={'margin-left': '2%', 'margin-right': '2%', 'margin-top': '15px'})
 
+server = app.server
+
 if __name__ == '__main__':
 	app.run_server(
         host='0.0.0.0',
